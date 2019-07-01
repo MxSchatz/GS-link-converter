8570bc84a10d42c7c78804fe61786d4d0945d3b7
Winter <a.schatz.is@gmail.com>
2019-07-01 13:34:54 -0400

    Initial Upload

---
+++ alltheaction.js
@@ -0,0 +1,41 @@
+
+
+function goGrabIt() {
+
+  //show results
+  document.getElementById("demResultsMayne").style.display = "block";
+
+  path = document.getElementById("winLink").value;
+  path = path.replace(/\\/g, "/");
+  path = path.replace("Z:/");
+  path = path.split(' ').join("%20");
+  var drive = /(.)\:\//;
+
+  //Turn into URL object
+  var uri = new URL(`smb://10.60.50.20/Data/${path}`).href;
+  var plainText = document.createTextNode(uri);
+
+  //sore variable for if statement
+  var thing = document.getElementById('clickResults').textContent;
+
+  if (thing.length > 1) {
+
+        //remove current text if there is any
+        document.getElementById('clickResults').removeChild(resultsLink);
+
+        //deliver to div
+        document.getElementById('clickResults').innerHTML = '<a id="resultsLink" href="'+uri+'"></a>';
+        document.getElementById('resultsLink').appendChild(plainText);
+
+    } else {
+
+      //deliver to div
+      document.getElementById('clickResults').innerHTML = '<a id="resultsLink" href="'+uri+'"></a>';
+      document.getElementById('resultsLink').appendChild(plainText);
+  }
+
+}
+
+  function clearIt(){
+    document.getElementById('winLink').value = '';
+  }
