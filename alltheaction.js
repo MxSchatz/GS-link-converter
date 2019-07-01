d68e1e00ed70e16bc1c9e378c585e389c9ad3d7f
Winter <a.schatz.is@gmail.com>
2019-07-01 15:37:43 -0400

    Doug Little efficiencies update
    Overhaul with Doug Little's tidy code

--- alltheaction.js
+++ alltheaction.js
@@ -1,13 +1,57 @@
+var submitBtn=document.querySelector('#submit-path');
+submitBtn.addEventListener('click',submitPath);
 
+function submitPath(e){
+var userPath=document.querySelector('#file-path').value;
+var path = String.raw`${userPath}`;
+
+document.getElementById("demResultsMayne").style.display = "block";
+// Tidying
+path = path.replace(/\\/g, "/");
+path=path.split(' ').join("%20");
+path=path.replace('Z:\/',"");
+
+//Turn into URL object
+var uri = new URL(`smb://10.60.50.20/Data/${path}`).href;
+var plainText = document.createTextNode(uri);
+
+// Send to Clipboard
+const el=document.createElement('textarea');
+el.value=uri;
+document.body.appendChild(el);
+el.select();
+document.execCommand("copy");
+document.body.removeChild(el);
+
+var thing = document.getElementById('clickResults').textContent;
+if (thing.length > 1) {
+
+        //remove current text if there is any
+        document.getElementById('clickResults').removeChild(resultsLink);
+
+        //deliver to div
+        document.getElementById('clickResults').innerHTML = '<a id="resultsLink" href="'+uri+'"></a>';
+        document.getElementById('resultsLink').appendChild(plainText);
+
+    } else {
+
+      //deliver to div
+      document.getElementById('clickResults').innerHTML = '<a id="resultsLink" href="'+uri+'"></a>';
+      document.getElementById('resultsLink').appendChild(plainText);
+  }
+
+}
 
 function goGrabIt() {
 
   //show results
   document.getElementById("demResultsMayne").style.display = "block";
 
-  path = document.getElementById("winLink").value;
+  // var  = document.getElementById("winLink").value;
+  var path = document.getElementById("winLink").value;
+
   path = path.replace(/\\/g, "/");
-  path = path.replace("Z:");
+  path = path.replace("Z:/");
   path = path.split(' ').join("%20");
   var drive = /(.)\:\//;
 
@@ -40,5 +84,5 @@ function goGrabIt() {
 }
 
   function clearIt(){
-    document.getElementById('winLink').value = '';
+    document.getElementById('file-path').value = '';
   }
